fe5626a8c8c31278a0b5d06db96a0849d718a217
Willian Galvani <williangalvani@gmail.com>
2025-04-20 12:31:44 -0300

    tweaks

--- scripts/world.gd
+++ scripts/world.gd
@@ -26,6 +26,10 @@ var players_loaded = 0
 
 var player_scene = preload("res://vehicles/bluerov2/BlueROV2.tscn")
 
+func pprint(string: String):
+	var id = multiplayer.get_unique_id()
+	print("%s: %s" % [id, string])
+
 func _ready():
 	if is_server:
 		$serverCamera.make_current()
@@ -41,14 +45,14 @@ func _ready():
 		join_game()
 
 func join_game(address = ""):
-	print("joining")
+	self.pprint("joining")
 	if address.is_empty():
 		address = DEFAULT_SERVER_IP
 	
 	var peer = WebSocketMultiplayerPeer.new()
 	# Always use ws:// protocol (not wss://) to ensure TLS is disabled
 	var url = "ws://%s:%s" % [address, PORT]
-	print("Connecting to: ", url)
+	pprint("Connecting to: %s" % url)
 	
 	var error = peer.create_client(url)
 	if error:
@@ -56,7 +60,7 @@ func join_game(address = ""):
 		return error
 	multiplayer.multiplayer_peer = peer
 
-	await get_tree().create_timer(2.0).timeout 
+	await get_tree().create_timer(1.0).timeout 
 	var id = str(multiplayer.get_unique_id())
 	# Loop through children to find the one matching our ID
 	var my_bluerov = null
@@ -64,9 +68,7 @@ func join_game(address = ""):
 		if child.name == id:
 			my_bluerov = child
 			break
-	print($players.get_children())
-	print("my name", id)
-	print("my rov:", my_bluerov)
+	#print($players.get_children())
 	%PlayerPhantomCamera3D.follow_target = my_bluerov
 	
 	# If we found our vehicle, look for a camera in its children and make it active
@@ -118,67 +120,53 @@ func create_game():
 	
 	print("WebSocket server created successfully on port: ", used_port)
 	multiplayer.multiplayer_peer = peer
-	
-	players[1] = player_info
-	player_connected.emit(1, player_info)
-
 
 func remove_multiplayer_peer():
 	multiplayer.multiplayer_peer = null
 	players.clear()
 
-
-# When the server decides to start the game from a UI scene,
-# do Lobby.load_game.rpc(filepath)
-@rpc("call_local", "reliable")
-func load_game(game_scene_path):
-	get_tree().change_scene_to_file(game_scene_path)
-
-
-# Every peer will call this when they have loaded the game scene.
-@rpc("any_peer", "call_local", "reliable")
-func player_loaded():
-	if multiplayer.is_server():
-		players_loaded += 1
-		if players_loaded == players.size():
-			$/root/Game.start_game()
-			players_loaded = 0
-
-
 # When a peer connects, send them my player info.
 # This allows transfer of all desired data for each player, not only the unique ID.
 func _on_player_connected(id):
+	player_info["peerid"] = multiplayer.get_unique_id()
+	player_info["simple_id"] = players.size()
+	players[id] = player_info
 	_register_player.rpc_id(id, player_info)
 	var new_player = player_scene.instantiate()
 	new_player.name = str(id)
 	$players.add_child(new_player, true)
 	new_player.set_id(id)
-	print("player %s connected" % id)
+	pprint("player %s connected" % id)
 
-
+# this runs on clients
 @rpc("any_peer", "reliable")
 func _register_player(new_player_info):
 	var new_player_id = multiplayer.get_remote_sender_id()
 	players[new_player_id] = new_player_info
 	player_connected.emit(new_player_id, new_player_info)
-
+	pprint("player %s registered" % new_player_info)
 
 func _on_player_disconnected(id):
 	players.erase(id)
 	player_disconnected.emit(id)
-
+	pprint("player %s disconnected" % id)
+	for child in $players.get_children():
+		if child.name == str(id):
+			child.queue_free()
+			break
 
 func _on_connected_ok():
 	var peer_id = multiplayer.get_unique_id()
 	players[peer_id] = player_info
 	player_connected.emit(peer_id, player_info)
-
+	pprint("player %s connected" % peer_id)
 
 func _on_connected_fail():
 	multiplayer.multiplayer_peer = null
-
+	pprint("connection failed")
 
 func _on_server_disconnected():
 	multiplayer.multiplayer_peer = null
 	players.clear()
 	server_disconnected.emit()
+	pprint("server disconnected")
