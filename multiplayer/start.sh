a2fe6b2624f4573f54ca04f5750c03c2831980f3
Willian Galvani <williangalvani@gmail.com>
2025-10-28 14:21:03 -0300

    update start.sh

--- multiplayer/start.sh
+++ multiplayer/start.sh
@@ -15,10 +15,10 @@ cp eeprom.bin bag-of-holding blueos3/
 cp eeprom.bin bag-of-holding blueos4/
 
 # now, for each file, replace $IFRAME_URL with the base url, followed by `?player=1`, `?player=2`, `?player=3`, or `?player=4`
-sed -i '' "s,\$IFRAME_URL,$1?player=1," blueos1/bag-of-holding
-sed -i '' "s,\$IFRAME_URL,$1?player=2," blueos2/bag-of-holding
-sed -i '' "s,\$IFRAME_URL,$1?player=3," blueos3/bag-of-holding
-sed -i '' "s,\$IFRAME_URL,$1?player=4," blueos4/bag-of-holding
+sed -i "s,\$IFRAME_URL,$1?player=1," blueos1/bag-of-holding
+sed -i "s,\$IFRAME_URL,$1?player=2," blueos2/bag-of-holding
+sed -i "s,\$IFRAME_URL,$1?player=3," blueos3/bag-of-holding
+sed -i "s,\$IFRAME_URL,$1?player=4," blueos4/bag-of-holding
 
 # then start the docker compose
 docker compose up -d
